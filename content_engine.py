8b573946a40ed99f7213f1b37374cbaab0b81768
veda-aarushi <121644311+veda-aarushi@users.noreply.github.com>
2025-05-17 00:38:20 -0700

    chore: remove TensorFlow autoencoder, revert to TF-IDF only

--- content_engine.py
+++ content_engine.py
@@ -7,19 +7,6 @@ import config
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
-
-
-def build_autoencoder(input_dim: int, latent_dim: int):
-    inputs = Input(shape=(input_dim,))
-    encoded = Dense(latent_dim, activation="relu")(inputs)
-    decoded = Dense(input_dim, activation="sigmoid")(encoded)
-    autoencoder = Model(inputs, decoded)
-    encoder = Model(inputs, encoded)
-    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
-    return autoencoder, encoder
-
 
 class ContentEngine:
     SIMKEY = "p:smlr:%s"
@@ -27,30 +14,14 @@ class ContentEngine:
     def __init__(self, redis_url: str = config.REDIS_URL):
         self._r = redis.from_url(redis_url)
         self.df = None
-        self.last_csv = None
 
     def train(self, csv_path: str):
         df = pd.read_csv(csv_path)
         self.df = df
-        self.last_csv = csv_path
 
         tfidf = TfidfVectorizer(ngram_range=(1, 3), stop_words="english")
         matrix = tfidf.fit_transform(df["description"])
-
-        # choose model type
-        if config.MODEL_TYPE == "autoencoder":
-            X = matrix.toarray()
-            autoencoder, encoder = build_autoencoder(X.shape[1], config.LATENT_DIM)
-            autoencoder.fit(
-                X, X,
-                epochs=config.AE_EPOCHS,
-                batch_size=config.AE_BATCH_SIZE,
-                verbose=0
-            )
-            embeddings = encoder.predict(X)
-            sims = linear_kernel(embeddings, embeddings)
-        else:
-            sims = linear_kernel(matrix, matrix)
+        sims = linear_kernel(matrix, matrix)
 
         self._r.flushdb()
         for idx, row in df.iterrows():
